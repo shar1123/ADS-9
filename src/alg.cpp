0b04a4cf6ac4d84ef3eac383583db28cd42e83d8
shar1123 <shchelkunova.arina2314@mail.ru>
2025-06-08 00:35:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -78,7 +78,7 @@ std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
 
 std::vector<char> getPerm1(PMTree& tree, int num) {
   auto perms = getAllPerms(tree);
-  if (num <= 0 || num > (int)perms.size())
+  if (num <= 0 || num > static_cast<int>(perms.size()))
     return {};
   return perms[num - 1];
 }
